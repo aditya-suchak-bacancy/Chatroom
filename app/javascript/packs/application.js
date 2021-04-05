96b118719998759c382d70e9051d23d7da67ae9a
Aditya <aditya.suchak@bacancy.com>
2021-04-05 11:29:46 +0530

    Add Semantic UI, Add Nav bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require jquery_ujs
+//= require semantic-ui
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -11,3 +14,8 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require("semantic-ui-sass")
+
+$(document).on('turbolinks:load', function(){
+  $('.ui.dropdown').dropdown();
+})
