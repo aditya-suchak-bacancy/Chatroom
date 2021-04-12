3d3e3b3ce2a03fdd1091e9a78d3b4be7197590bf
Aditya <aditya.suchak@bacancy.com>
2021-04-12 23:13:10 +0530

    Add messages to UI using action cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,19 @@
 class MessagesController < ApplicationController
   def create
-    message = current_user.messages.build(params.require(:message).permit(:text))
+    message = current_user.messages.build(message_params)
     if message.save
-      redirect_to root_path
+      ActionCable.server.broadcast "chatroom_channel",
+                                    foo: message_render(message)
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
   end
 end
--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -10,6 +10,7 @@ consumer.subscriptions.create("ChatroomChannel", {
   },
 
   received(data) {
+    $('#message-container').append(data.foo);
     // Called when there's incoming data on the websocket for this channel
   }
 });
