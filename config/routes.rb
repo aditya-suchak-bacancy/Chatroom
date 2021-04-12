44e988c83f3d8fc3395b41959196299819df069c
Aditya <aditya.suchak@bacancy.com>
2021-04-12 22:07:03 +0530

    set up a action cable/generate a channel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'chatroom#index'
   devise_for :users
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
