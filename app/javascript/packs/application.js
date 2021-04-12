06bc3672d045b4fe0abcf37970adf85586b9f215
Aditya <aditya.suchak@bacancy.com>
2021-04-13 00:08:31 +0530

    Add autoscroll to a js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,14 +15,8 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 require("semantic-ui-sass")
-
-scroll_bottom = function() {
-  if ($('#messages').length > 0){
-    $('#messages').scrollTop($('#messages')[0].scrollHeight);
-  }
-}
+require("scrollup")
 
 $(document).on('turbolinks:load', function(){
   $('.ui.dropdown').dropdown();
-  scroll_bottom();
 })
---
+++ app/javascript/scrollup.js
@@ -0,0 +1,5 @@
+function scroll_bottom() {
+  if ($('#messages').length > 0){
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
